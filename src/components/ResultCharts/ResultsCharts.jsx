413a4ca84a7d0137f2cc3049947ca951ed7dc43b
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-20 20:22:02 +0600

    addad buy charts in this file

--- src/components/ResultCharts/ResultsCharts.jsx
+++ src/components/ResultCharts/ResultsCharts.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import { Line, LineChart } from "recharts";
+import { ComposedChart, Line, LineChart, XAxis, YAxis } from "recharts";
 
 const ResultsCharts = () => {
   const salesData = [
-    { month: "Jan", sales: 1200 },
-    { month: "Feb", sales: 2100 },
-    { month: "Mar", sales: 1800 },
-    { month: "Apr", sales: 2600 },
-    { month: "may", sales: 1800 },
-    { month: "jun", sales: 1500 },
-    { month: "jul", sales: 3000 },
-    { month: "aug", sales: 3200 },
+    { month: "Jan", sales: 1200, buy: 700 },
+    { month: "Feb", sales: 2100, buy: 1500 },
+    { month: "Mar", sales: 1800, buy: 2000 },
+    { month: "Apr", sales: 2600, buy: 1800 },
+    { month: "may", sales: 1800, buy: 2200 },
+    { month: "jun", sales: 1500, buy: 1000 },
+    { month: "jul", sales: 3000, buy: 2500 },
+    { month: "aug", sales: 3200, buy: 1500 },
   ];
   return (
     <div>
       <LineChart width={800} height={500} data={salesData}>
-        <Line dataKey={"sales"}></Line>
+        <XAxis dataKey={"month"}></XAxis>
+        <YAxis></YAxis>
+        <Line dataKey="buy" stroke="red"></Line>
+        <Line dataKey={"sales"} stroke="green"></Line>
       </LineChart>
     </div>
   );
