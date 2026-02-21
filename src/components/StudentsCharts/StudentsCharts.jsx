e8c51400f2d0119663616b246a911a01bd1cd439
sohagroy7223 <freelancersohagroy@gmail.com>
2026-02-21 11:36:55 +0600

    addad full charts final output by axios call and change tooltip hover effect

--- src/components/StudentsCharts/StudentsCharts.jsx
+++ src/components/StudentsCharts/StudentsCharts.jsx
@@ -1,13 +1,41 @@
 import React, { use } from "react";
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
 
 const StudentsCharts = ({ studentsData }) => {
   const studentsDetails = use(studentsData);
   const allStudentData = studentsDetails.data;
 
-  console.log(allStudentData);
+  //   data processing for the charts
+
+  const studentsChartData = allStudentData.map((studentData) => {
+    const student = {
+      id: studentData.id,
+      name: studentData.name,
+      roll: studentData.roll,
+      class: studentData.class,
+      section: studentData.section,
+      math: studentData.marks.math,
+      english: studentData.marks.english,
+      science: studentData.marks.science,
+      total: studentData.total,
+      grade: studentData.grade,
+    };
+    return student;
+  });
+
+  //   console.log(studentsChartData);
+
   return (
     <div>
-      <h3>hello{allStudentData.length}</h3>
+      <BarChart width={500} height={300} data={studentsChartData}>
+        <XAxis dataKey="name"></XAxis>
+        <YAxis></YAxis>
+        <Bar dataKey="total" fill="blue"></Bar>
+        <Bar dataKey={"math"} fill="green"></Bar>
+        <Tooltip
+          contentStyle={{ background: "#2f2937", borderRadius: "20px" }}
+        ></Tooltip>
+      </BarChart>
     </div>
   );
 };
